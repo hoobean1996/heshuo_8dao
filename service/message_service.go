9b54dedcd8b1c5e55c61f6f2204f22eeccc2f0fb
HEBIN <47626391+hoobean1996@users.noreply.github.com>
2025-02-15 20:34:41 +0800

    fix go version

--- service/message_service.go
+++ service/message_service.go
@@ -10,17 +10,31 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// TextMessage 定义接收的XML消息结构
-type TextMessage struct {
+type CommonMessage struct {
 	XMLName      xml.Name `xml:"xml"`
 	ToUserName   string   `xml:"ToUserName"`
 	FromUserName string   `xml:"FromUserName"`
 	CreateTime   int64    `xml:"CreateTime"`
 	MsgType      string   `xml:"MsgType"`
-	Content      string   `xml:"Content"`
-	MsgId        int64    `xml:"MsgId"`
-	MsgDataId    string   `xml:"MsgDataId"`
-	Idx          string   `xml:"Idx"`
+	// 文本消息特有
+	Content string `xml:"Content,omitempty"`
+	MsgId   int64  `xml:"MsgId,omitempty"`
+	// 事件消息特有
+	Event    string `xml:"Event,omitempty"`
+	EventKey string `xml:"EventKey,omitempty"`
+	// 图片消息特有
+	PicUrl  string `xml:"PicUrl,omitempty"`
+	MediaId string `xml:"MediaId,omitempty"`
+	// 语音消息特有
+	Format      string `xml:"Format,omitempty"`
+	Recognition string `xml:"Recognition,omitempty"`
+	// 视频/小视频消息特有
+	ThumbMediaId string `xml:"ThumbMediaId,omitempty"`
+	// 地理位置消息特有
+	Location_X float64 `xml:"Location_X,omitempty"`
+	Location_Y float64 `xml:"Location_Y,omitempty"`
+	Scale      int     `xml:"Scale,omitempty"`
+	Label      string  `xml:"Label,omitempty"`
 }
 
 type TextResponse struct {
@@ -44,7 +58,7 @@ func HandleTextMessage(c *gin.Context) {
 	}
 
 	// 解析XML消息
-	var message TextMessage
+	var message CommonMessage
 	if err := xml.Unmarshal(body, &message); err != nil {
 		c.XML(http.StatusBadRequest, gin.H{
 			"error": "Failed to parse XML message",
